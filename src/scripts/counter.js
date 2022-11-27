10211ca2d29eb8c56ab5067160e68d58f1febc03
Dmitry Maevsky <dmitry.maevsky@gmail.com>
2022-11-27 14:19:58 +0900

    first "nice" version

--- src/scripts/counter.js
+++ src/scripts/counter.js
@@ -2,3 +2,4 @@ import { writable } from 'tinyx';
 
 export const count = writable(0);
 export const increaseCount = () => count.update(c => c + 1);
+export const decreaseCount = () => count.update(c => c - 1);
