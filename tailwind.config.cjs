ebd2786d0effed83b8e9274d871bd4020a142e1e
Dmitry Maevsky <dmitry.maevsky@gmail.com>
2022-12-24 17:05:26 +0900

    use tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#237eb3'
+      }
+    },
+  },
+  plugins: [],
+  darkMode: 'class'
+}
